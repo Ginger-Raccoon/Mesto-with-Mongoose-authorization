953fa2cd38401aded92622ea6fd8cb026278ef92
Ilya Kosmachev <kosmachev.ie@gmail.com>
2020-09-01 13:28:11 +0300

    fix auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,9 @@ const jwt = require('jsonwebtoken');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  // const { authorization } = req.headers;
-
-  // if (!authorization || !authorization.startsWith('Bearer ')) {
-  //   return res.status(401).send({ message: 'Необходима авторизация' });
-  // }
-  // const token = authorization.replace('Bearer ', '');
+  if (!req.cookies.jwt) {
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
   const token = req.cookies.jwt;
   let payload;
   try {
